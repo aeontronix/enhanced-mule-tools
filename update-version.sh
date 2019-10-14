ced8edbef791a58767b12a2666730988b3bc5252
Yannick Menager <4215831-ymenager@users.noreply.gitlab.com>
2019-10-13 21:28:45 -0700

    preparing for release

---
+++ update-version.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+    export POM_VERSION=$( xmlstarlet sel -N 'p=http://maven.apache.org/POM/4.0.0' -t -v '/p:project/p:version/text()' pom.xml | sed 's/-SNAPSHOT$//' );
+echo "Current version: ${POM_VERSION}"
+echo "New version ?"
+read VERSION
+mvn versions:set -DprocessAllModules=true -DgroupId='*' -DartifactId='*' -DnewVersion=${VERSION}-SNAPSHOT
