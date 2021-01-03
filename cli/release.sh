009b6075b65bd1c676a33e8fd9eb5cc4158cf0e7
Yannick Menager <4215831-ymenager@users.noreply.gitlab.com>
2021-01-03 15:36:30 -0800

    support api spec assets, plus various refactorings and release scripting

--- cli/release.sh
+++ cli/release.sh
@@ -8,19 +8,17 @@ fi
 FILE=test.txt
 
 RELVERSION=${POM_VERSION}
-DIST_SHA=($(shasum -a 256 cli/target/enhanced-mule-tools-cli-1.3.0-alpha3-SNAPSHOT.jar))
+DIST_SHA=($(shasum -a 256 cli/target/enhanced-mule-tools-cli-${RELVERSION}.jar))
 
-sed -e "s/@version@/${RELVERSION}/g" -e "s/@checksum@/${DIST_SHA}/" cli/src/main/assembly/emt.tap.rb
+sed -e "s/@version@/${RELVERSION}/g" -e "s/@checksum@/${DIST_SHA}/" cli/src/main/assembly/emt.tap.rb >cli/target/emt.rb
+
+CONTENT=$(openssl base64 -A -in cli/target/emt.rb)
 
 URL=https://api.github.com/repos/enhanced-mule/homebrew-tools/contents/${FILE}
 
 SHA=$(curl -s -X GET ${URL} | jq -r .sha)
 
-echo ${TAP_ENCODED}
-
-JSON="{\"path\": \"${FILE}\", \"message\": \"update\", \"content\": \"cmVxdWlyZSAiZm9ybXVsYSIgY2xhc3MgRW10IDwgRm9ybXVsYSBkZXNjICJFbmhh bmNlZCBNdWxlIENMSSIgaG9tZXBhZ2UgImh0dHBzOi8vd3d3LmVuaGFuY2VkLW11 bGUuY29tLyIgdXJsICJodHRwczovL3JlcG8xLm1hdmVuLm9yZy9tYXZlbjIvY29t L2Flb250cm9uaXgvZW5oYW5jZWQtbXVsZS9lbmhhbmNlZC1tdWxlLXRvb2xzLWNs aS8vZW5oYW5jZWQtbXVsZS10b29scy1jbGktLWRpc3QudGJ6MiIgc2hhMjU2ICJl YTY4YTA5MjEyZDExMzFiMjJiYzFhMjM0M2UwM2E4ODAwODc0NmFhNDMzZjY4ZThi MzkwNDAyOTFjZWU2OWZkIiBoZWFkICJodHRwczovL2dpdGh1Yi5jb20vZW5oYW5j ZWQtbXVsZS9ob21lYnJldy10b29scy5naXQiIGRlZiBpbnN0YWxsIGxpYmV4ZWMu aW5zdGFsbCAiYmluIiwgImxpYiIsICJSRUFETUUubWQiIGJpbi53cml0ZV9leGVj X3NjcmlwdCBsaWJleGVjLyJiaW4vZW10IiBlbmQgdGVzdCBkbyBhc3NlcnRfbWF0 Y2ggIiIsIHNoZWxsX291dHB1dCgiI3tiaW59L2VtdCAtViIsIDIpIGVuZCBlbmQK\", \"branch\": \"master\", \"sha\": \"${SHA}\" }"
-
-echo $JSON
+JSON="{\"path\": \"${FILE}\", \"message\": \"update\", \"content\": \"${CONTENT}\", \"branch\": \"master\", \"sha\": \"${SHA}\" }"
 
 curl --request PUT \
   --url ${URL} \
