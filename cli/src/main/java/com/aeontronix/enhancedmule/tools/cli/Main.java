11af4fb903844bc6e298a5cb9131cd82c980e8f3
Yannick Menager <4215831-ymenager@users.noreply.gitlab.com>
2021-01-02 20:40:31 -0800

    Changed version and fixed rel build

---
+++ cli/src/main/java/com/aeontronix/enhancedmule/tools/cli/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Aeontronix 2021
+ */
+
+package com.aeontronix.enhancedmule.tools.cli;
+
+import com.aeontronix.enhancedmule.tools.cli.EMTCli;
+import com.aeontronix.enhancedmule.tools.cli.application.ApplicationCmd;
+import picocli.CommandLine;
+
+public class Main {
+    public static void main(String[] args) {
+        final EMTCli cli = new EMTCli();
+        final CommandLine commandLine = new CommandLine(cli);
+        commandLine.addSubcommand("application",new ApplicationCmd());
+        commandLine.setColorScheme(CommandLine.Help.defaultColorScheme(CommandLine.Help.Ansi.ON));
+        commandLine.setUsageHelpAutoWidth(true);
+        commandLine.setCaseInsensitiveEnumValuesAllowed(true);
+        int exitCode = commandLine.execute(args);
+        System.exit(exitCode);
+    }
+}
--- lib/src/main/java/com/aeontronix/enhancedmule/tools/cli/EMTCli.java
+++ lib/src/main/java/com/aeontronix/enhancedmule/tools/cli/EMTCli.java
@@ -4,21 +4,9 @@
 
 package com.aeontronix.enhancedmule.tools.cli;
 
-import com.aeontronix.enhancedmule.tools.cli.application.ApplicationCmd;
 import com.aeontronix.enhancedmule.tools.util.VersionHelper;
-import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 @Command(name = "emt",mixinStandardHelpOptions = true,versionProvider = VersionHelper.class)
 public class EMTCli {
-    public static void main(String[] args) {
-        final EMTCli cli = new EMTCli();
-        final CommandLine commandLine = new CommandLine(cli);
-        commandLine.addSubcommand("application",new ApplicationCmd());
-        commandLine.setColorScheme(CommandLine.Help.defaultColorScheme(CommandLine.Help.Ansi.ON));
-        commandLine.setUsageHelpAutoWidth(true);
-        commandLine.setCaseInsensitiveEnumValuesAllowed(true);
-        int exitCode = commandLine.execute(args);
-        System.exit(exitCode);
-    }
 }
