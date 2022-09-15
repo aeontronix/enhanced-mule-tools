5c046b9f6ab73a7f17d35df3dbd786eccc9065f6
Yannick Menager <4215831-ymenager@users.noreply.gitlab.com>
2022-09-14 23:50:03 -0700

    changed version

--- maven-plugin/src/main/java/com/aeontronix/enhancedmule/tools/DecryptPropertiesMojo.java
+++ maven-plugin/src/main/java/com/aeontronix/enhancedmule/tools/DecryptPropertiesMojo.java
@@ -7,11 +7,15 @@ package com.aeontronix.enhancedmule.tools;
 import com.aeontronix.enhancedmule.tools.cli.crypto.CryptoHelper;
 import com.aeontronix.kryptotek.Key;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Mojo(name = "decrypt", requiresProject = false)
 public class DecryptPropertiesMojo extends AbstractCryptoMojo {
+    private static final Logger logger = getLogger(DecryptPropertiesMojo.class);
     @Override
     protected void execute(Key key, String value) throws Exception {
-        System.out.println(CryptoHelper.decrypt(key, value));
+        logger.info("Decrypted Value: " + CryptoHelper.decrypt(key, value));
     }
 }
