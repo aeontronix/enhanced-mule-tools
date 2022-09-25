7ef821679a0abbd50f3000fb0ba51ede0e58bb3d
Yannick Menager <4215831-ymenager@users.noreply.gitlab.com>
2022-09-25 01:14:34 -0700

    Implemented properties encryption

--- cli/src/main/assembly/emt.tap-snapshot.rb
+++ cli/src/main/assembly/emt.tap-snapshot.rb
@@ -3,7 +3,7 @@ require "formula"
 class Emt < Formula
   desc "Enhanced Mule Tools CLI"
   homepage "https://www.enhanced-mule.com"
-  url "https://repo1.maven.org/maven2/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/@version@/enhanced-mule-tools-cli-@version@-dist.tbz2"
+  url "https://oss.sonatype.org/content/repositories/snapshots/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/@version@/enhanced-mule-tools-cli-@version@-dist.tbz2"
   sha256 "@checksum@"
   head "https://github.com/enhanced-mule/homebrew-tools.git"
 
