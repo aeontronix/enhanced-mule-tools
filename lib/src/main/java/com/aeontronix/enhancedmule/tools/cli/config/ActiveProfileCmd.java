af40bfcea2ad09fb2dada919d200a54e654254bc
Yannick Menager <4215831-ymenager@users.noreply.gitlab.com>
2022-09-30 12:13:57 -0700

    Fixed broken ActiveProfileCmd

--- lib/src/main/java/com/aeontronix/enhancedmule/tools/cli/config/ActiveProfileCmd.java
+++ lib/src/main/java/com/aeontronix/enhancedmule/tools/cli/config/ActiveProfileCmd.java
@@ -10,19 +10,20 @@ import org.fusesource.jansi.Ansi;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+import picocli.CommandLine.ParentCommand;
 
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "profile", description = "Change or show active profile")
 public class ActiveProfileCmd implements Callable<Integer> {
+    @ParentCommand
     private EMTCli cli;
     @Option(names = {"-c", "--create"}, description = "Create profile if required")
     boolean create;
     @Parameters(arity = "0..1", description = "profile to activate")
     private String profile;
 
-    public ActiveProfileCmd(EMTCli cli) {
-        this.cli = cli;
+    public ActiveProfileCmd() {
     }
 
     @Override
