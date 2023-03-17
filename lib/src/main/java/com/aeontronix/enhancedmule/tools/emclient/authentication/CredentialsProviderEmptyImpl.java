ba578de4e5170e1127c5d12ca2a7f410461b3c80
Yannick Menager <4215831-ymenager@users.noreply.gitlab.com>
2023-03-16 17:54:58 -0700

    Fixed deploy failing when no credentials present AND deploy skip is used

--- lib/src/main/java/com/aeontronix/enhancedmule/tools/emclient/authentication/CredentialsProviderEmptyImpl.java
+++ lib/src/main/java/com/aeontronix/enhancedmule/tools/emclient/authentication/CredentialsProviderEmptyImpl.java
@@ -7,6 +7,8 @@ package com.aeontronix.enhancedmule.tools.emclient.authentication;
 import com.aeontronix.enhancedmule.tools.authentication.Credentials;
 import com.aeontronix.enhancedmule.tools.emclient.EnhancedMuleClient;
 import com.aeontronix.restclient.RESTClient;
+import com.aeontronix.restclient.RESTException;
+import com.aeontronix.restclient.RESTRequest;
 import com.aeontronix.restclient.auth.AuthenticationHandler;
 
 import java.io.IOException;
@@ -14,7 +16,7 @@ import java.io.IOException;
 public class CredentialsProviderEmptyImpl implements AnypointBearerTokenCredentialsProvider {
     @Override
     public String getAnypointBearerToken(EnhancedMuleClient restClient) throws IOException {
-        throw new IOException("No credentials provided");
+        return "NOBEARER";
     }
 
     @Override
@@ -24,6 +26,25 @@ public class CredentialsProviderEmptyImpl implements AnypointBearerTokenCredenti
 
     @Override
     public AuthenticationHandler toAuthenticationHandler(RESTClient restClient, String anypointPlatformUrl) {
-        throw new RuntimeException("No credentials provided");
+        return new AuthenticationHandler() {
+            @Override
+            public boolean isRefreshRequired() {
+                return false;
+            }
+
+            @Override
+            public boolean isRefreshable() {
+                return false;
+            }
+
+            @Override
+            public void applyCredentials(RESTRequest request) {
+                throw new RuntimeException("No credentials provided");
+            }
+
+            @Override
+            public void refreshCredential(RESTClient restClient) throws RESTException {
+            }
+        };
     }
 }
